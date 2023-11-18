5903beaadaf18fd37789cde0ab856705d10ad59a
dew-9 <add.amnesia@gmail.com>
2023-11-18 16:10:10 -0500

    Added search functionality with API

--- src/script.js
+++ src/script.js
@@ -1,10 +1,25 @@
+function refreshWeather(response) {
+  let currentTemp = document.querySelector("#currentTemp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  currentTemp.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "6o808f4d3ta99b430e5547b006a7c43c";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-box");
-  console.log(searchInput.value);
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("Hamilton");
