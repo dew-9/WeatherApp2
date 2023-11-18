71c79fe2aa580848cb30178a60177a8aa2d64785
dew-9 <add.amnesia@gmail.com>
2023-11-18 16:53:58 -0500

    Injected more weather and time data

--- src/script.js
+++ src/script.js
@@ -2,8 +2,39 @@ function refreshWeather(response) {
   let currentTemp = document.querySelector("#currentTemp");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let percipitationElement = document.querySelector("#percipitation");
+  console.log(response.data);
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
   cityElement.innerHTML = response.data.city;
   currentTemp.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  percipitationElement.innerHTML = `Percipitation: ${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `Wind: ${response.data.wind.speed} km/h`;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hour = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hour}:${minutes}`;
 }
 
 function searchCity(city) {
